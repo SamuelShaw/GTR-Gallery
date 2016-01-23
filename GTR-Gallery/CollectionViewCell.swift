3fad81f9e7200095666a1050a8a7bd8edec53bf9
NeoTerminal <samyy.abcd@gmail.com>
2016-01-23 13:07:15 -0500

    configured collection view cell

---
+++ GTR-Gallery/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  GTR-Gallery
+//
+//  Created by Samuel Shaw on 1/23/16.
+//  Copyright © 2016 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
